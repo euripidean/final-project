09aafe0cfcba0f1c2e5fdc7c98a0894da317d481
Jane Harrison <33559193+euripidean@users.noreply.github.com>
2024-04-26 15:04:53 -0700

    Reading data from spreadsheet and marshalling into JSON

---
+++ main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"google.golang.org/api/option"
+	"google.golang.org/api/sheets/v4"
+)
+
+
+func main() {
+    // load the .env file
+   if err := godotenv.Load(); err != nil {
+        log.Fatal("Error loading .env file")
+    }
+
+    // set API key
+	apiKey := os.Getenv("API_KEY")
+
+    // context for connection
+    ctx := context.Background()
+
+    // connect to sheets client
+    service, err := sheets.NewService(ctx, option.WithAPIKey(apiKey))
+    if err != nil {
+        log.Fatalf("Unable to retrieve Sheets client: %v", err)
+    }
+
+    // The ID of the spreadsheet you want to access. TODO: stretch goal to maybe get multiple spreadsheets from a folder?
+    spreadsheetID := os.Getenv("SPREADSHEET_ID")
+
+    // This way of implementing requires a range for the Get method. Meep moop.
+    readRange := "Sheet1!A1:Z"
+
+    fmt.Println("This is just here to keep fmt as an import when I save.")
+
+   
+
+    resp, err := service.Spreadsheets.Values.Get(spreadsheetID, readRange).Do()
+    if err != nil {
+        log.Fatalf("Unable to retrieve data from sheet: %v", err)
+    }
+
+    var data []map[string]interface{}
+
+if len(resp.Values) > 0 {
+    // Get the headers from the first row
+    var headers []string
+    for _, header := range resp.Values[0] {
+        headers = append(headers, header.(string))
+    }
+
+    // Get the data from the remaining rows
+    for _, row := range resp.Values[1:] {
+        rowData := make(map[string]interface{})
+        for i, cell := range row {
+            if i < len(headers) {
+                rowData[headers[i]] = cell
+            }
+        }
+        data = append(data, rowData)
+    }
+}
+
+// Convert the data to JSON
+jsonData, err := json.Marshal(data)
+if err != nil {
+    log.Fatalf("Unable to convert data to JSON: %v", err)
+}
+
+file, err := os.Create("data.json")
+if err != nil {
+    log.Fatalf("Failed to create file: %v", err)
+}
+defer file.Close()
+
+writer := bufio.NewWriter(file)
+_, err = writer.WriteString(string(jsonData))
+if err != nil {
+    log.Fatalf("Failed to write to file: %v", err)
+}
+
+// Ensure all operations have been applied to the underlying writer
+if err := writer.Flush(); err != nil {
+    log.Fatalf("Failed to flush writer: %v", err)
+}
+
+}
